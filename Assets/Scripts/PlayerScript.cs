5566744ee8b1b4e4b63f2c5a40fab4c13fd4d63e
JCsama1 <james.cutajar.e21616@mcast.edu.mt>
2022-12-22 17:24:42 +0100

    4th Commit
    I finished the movement . I finalized the movement of the player by adding the jumping and the sprinting mechanic. I had a small problem with the ground and the player where the player model sinks a bit into the ground. I fixed it by making the player controller and the body the same height and I added a layer to the ground.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
 
-    //Creates a field to arrange player speed
+    //Creates a field to arrange player speed and sprint
     [Header("Player Movement")]
     public float playerSpeed = 1.9f;
+    public float playerSprint = 3f;
 
-    //Creates a field to input the Character Controller
-    [Header("Player Animator and Gravity")]
-    public CharacterController cC;
+
+    //[Header("Player Health Things")]
 
     //Creates a field to input the MainCamera
     [Header("Player Script Cameras")]
     public Transform playerCamera;
 
+    //Creates a field to input the Character Controller
+    [Header("Player Animator and Gravity")]
+    public CharacterController cC;
+    public float gravity = -9.81f;
+
     [Header("Player Jumping and Velocity")]
+    public float jumpRange = 1f;
+    Vector3 velocity;
     public float turnCalmTime = 0.1f;
     float turnCalmVelocity;
+    public Transform surfaceCheck;
+    bool onSurface;
+    public float surfaceDistance = 0.4f;
+    public LayerMask surfaceMask;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +41,22 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        onSurface = Physics.CheckSphere(surfaceCheck.position, surfaceDistance, surfaceMask);
+
+        if(onSurface && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+        cC.Move(velocity * Time.deltaTime);
+
         playerMove();
+
+        Jump();
+
+        Sprint();
     }
 
     //Lets the player move by using arrow keys or WASD
@@ -54,4 +80,36 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && onSurface)
+        {
+            velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
+        }
+    }
+
+
+    //Lets the player move by using arrow keys or WASD
+    void Sprint()
+    {
+        if(Input.GetButton("Sprint") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && onSurface)
+        {
+            float horizontal_axis = Input.GetAxisRaw("Horizontal");
+            float vertical_axis = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                //This basically rotates the player model when moving to the sides and makes the rotation smoother.
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                //This lets the player turn towards mouse location.
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                cC.Move(moveDirection.normalized * playerSprint * Time.deltaTime);
+            }
+        }
+    }
 }
