0ebf1def3ee4173fd743e471210254ac7a6362bf
JCsama1 <james.cutajar.e21616@mcast.edu.mt>
2022-12-29 17:04:07 +0100

    8th Commit
    Today I implemented enemy AI. I set walk points and a radius around the enemy. When the player gets in the radius vicinity the enemy will respond (for now he stops his movement only).

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,9 +8,16 @@ public class Enemy : MonoBehaviour
 {
     //[Header("Enemy Health and Damage")]
 
-    //[Header("Enemy Things")]
+    [Header("Enemy Things")]
+    public NavMeshAgent EnemyAgent;
+    public Transform playerBody;
+    public LayerMask PlayerLayer;
 
-   //[Header("Enemy guarding Var")]
+    [Header("Enemy guarding Var")]
+    public GameObject[] walkPoints;
+    int currentEnemyPosition = 0;
+    public float enemySpeed;
+    float walkingpointRadius = 2;
 
     //[Header("Sounds and UI")]
 
@@ -17,5 +25,38 @@ public class Enemy : MonoBehaviour
 
     //[Header("Enemy Animation and Spark Effect")]
 
-    //[Header("Enemy mood/situation")]
+    [Header("Enemy mood/situation")]
+    public float visionRadius;
+    public float shootingRadius;
+    public bool playerInvisionRadius;
+    public bool playerInshootingRadius;
+
+    private void Awake()
+    {
+        playerBody = GameObject.Find("Player").transform;
+        EnemyAgent= GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInvisionRadius = Physics.CheckSphere(transform.position, visionRadius, PlayerLayer);
+        playerInshootingRadius = Physics.CheckSphere(transform.position, shootingRadius, PlayerLayer);
+
+        if (!playerInshootingRadius && !playerInvisionRadius) Guard();
+    }
+
+    private void Guard()
+    {
+        if (Vector3.Distance(walkPoints[currentEnemyPosition].transform.position, transform.position) < walkingpointRadius)
+        {
+            currentEnemyPosition = Random.Range(0, walkPoints.Length);
+            if(currentEnemyPosition >= walkPoints.Length)
+            {
+                currentEnemyPosition= 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, walkPoints[currentEnemyPosition].transform.position, Time.deltaTime * enemySpeed);
+        //changing enemy rotation
+        transform.LookAt(walkPoints[currentEnemyPosition].transform.position);
+    }
 }
